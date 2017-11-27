da948178d37dc7f0a7f18fabe6f5314b807f5f49
Evan Leri <m.evanleri@gmail.com>
2017-11-27 08:58:50 -0500

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function unlock() {
     window.location.href = 'welcome.html';
   }else {
     name = prompt('What is your name?');
-    if(name == 'Irel Nave' || name == 'Atomnus') {
+    if(name.toLowerCase() == 'Irel Nave'.toLowerCase() || name.toLowerCase() == 'Atomnus'.toLOwerCase()) {
       pass = prompt('VIP passcode?');
       if(pass == "283679908444") {
         window.location.href = 'welcome.html';
